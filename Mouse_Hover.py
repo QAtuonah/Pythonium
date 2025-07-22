b404ae8c9a4cb17a8686236297349fbdd6d1ad01
Christopher Atuonah <chr.dubem@gmail.com>
2025-07-22 15:52:17 -0300

    Update Mouse_Hover.py

--- Mouse_Hover.py
+++ Mouse_Hover.py
@@ -12,13 +12,13 @@ browser.get(url)
 # Hover mouse function
 actions = ActionChains(browser)
 
-# Find button element
+# Button to be hovered element
 hover_element = browser.find_element(By.XPATH,"//a[@class='no_border'][normalize-space()='Free Ebooks']")
 time.sleep(2)
 
-# Hover over button
+# Function to hover over the intended button
 actions.move_to_element(hover_element).perform()
 
-# Click on the button after hover
+# Identify and click on the button below the hovered button
 browser.find_element(By.XPATH,"//span[normalize-space()='Free Tensorflow eBooks']").click()
 time.sleep(5)
