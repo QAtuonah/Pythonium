16f2ecb50b4be74189ff424802cecf5452af1080
Christopher Atuonah <chr.dubem@gmail.com>
2025-07-22 13:54:59 -0300

    Create Mouse_Hover.py

---
+++ Mouse_Hover.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+import time
+
+# Open browser and url
+browser = webdriver.Chrome()
+browser.maximize_window()
+url = "https://www.globalsqa.com/demo-site/datepicker/"
+browser.get(url)
+
+# Hover mouse function
+actions = ActionChains(browser)
+
+# Find button element
+hover_element = browser.find_element(By.XPATH,"//a[@class='no_border'][normalize-space()='Free Ebooks']")
+time.sleep(2)
+
+# Hover over button
+actions.move_to_element(hover_element).perform()
+
+# Click on the button after hover
+browser.find_element(By.XPATH,"//span[normalize-space()='Free Tensorflow eBooks']").click()
+time.sleep(5)
